e74d676450a0c58252097dd2dc7bd6953b43429b
Valerie Rojdes <StefanValerie@Stefans-iMac.local>
2019-03-23 18:58:09 -0700

    last change

--- app/components/Main.js
+++ app/components/Main.js
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
       zIndex: 11,
       right:20,
       bottom: 70,
-      backgroundColor: 'blue',
+      backgroundColor: 'pink',
       width: 70,
       height: 70,
       borderRadius:50,
